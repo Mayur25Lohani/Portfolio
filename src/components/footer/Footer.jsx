ddbd5355fd5fe7104e3fabe00d2a1fbacb3468c8
Mayur25Lohani <lohanimayuresh2551@gmail.com>
2023-07-07 02:31:45 +0530

    Second Commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './footer.css'
 import {FaFacebook} from 'react-icons/fa'
+import {FaLinkedin} from 'react-icons/fa'
 import { FiInstagram } from 'react-icons/fi'
 import {IoLogoTwitter} from 'react-icons/io'
 
@@ -14,14 +15,14 @@ const Footer = () => {
         <li><a href="#experience">Experience</a></li>
         <li><a href="#services">Services</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
+        {/* <li><a href="#testimonials">Testimonials</a></li> */}
         <li><a href="#contact">Contact</a></li>
       </ul>
 
       <div className="footer__socials">
-        <a href="https://facebook.com"><FaFacebook /></a>
-        <a href="https://instagram.com"><FiInstagram /></a>
-        <a href="https://twitter.com"><IoLogoTwitter /></a>
+        <a href="https://www.facebook.com/profile.php?id=100075318804727"><FaFacebook /></a>
+        {/* <a href="https://instagram.com"><FiInstagram /></a> */}
+        <a href="www.linkedin.com/in/mayuresh-lohani-87b28922a"><FaLinkedin /></a>
       </div>
 
       <div className="footer__copyright">
